61661717d5f57c7cdc0f1e0d213ff4159e2ee6f7
Salih <salihozkara00@gmail.com>
2022-03-01 20:34:44 +0300

    create BaseDbContext.cs

---
+++ src/rentACar/Persistence/Context/BaseDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Context;
+
+public class BaseDbContext:DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+    public BaseDbContext(DbContextOptions options,IConfiguration configuration):base(options)
+    {
+        Configuration = configuration;
+    }
+
+    public DbSet<Brand> Brands { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(Configuration.GetConnectionString("RentACarConnectionString")));
+        }
+        
+    }
+}
