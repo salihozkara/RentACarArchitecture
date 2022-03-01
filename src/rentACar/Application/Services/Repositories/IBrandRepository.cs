5d3c453cc14652fef9a30ae5da4383d99e937384
Salih <salihozkara00@gmail.com>
2022-03-01 18:46:15 +0300

    fix name

--- src/rentACar/Application/Services/Repositories/IBrandRepository.cs
+++ src/rentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -3,7 +3,7 @@ using Domain.Entities;
 
 namespace Application.Services.Repositories;
 
-public interface IBrandService:IAsyncRepository<Brand>
+public interface IBrandRepository:IAsyncRepository<Brand>
 {
     
 }
