178540d084a23a62500ac3b3bdefdae88121ce18
Salih <salihozkara00@gmail.com>
2022-03-01 18:19:14 +0300

    add paginate

--- src/corePackages/Core.Persistence/Paging/IPaginate.cs
+++ src/corePackages/Core.Persistence/Paging/IPaginate.cs
@@ -1,1 +1,20 @@
-﻿
+﻿namespace Core.Persistence.Paging;
+
+public interface IPaginate<T>
+{
+    int From { get; }
+
+    int Index { get; }
+
+    int Size { get; }
+
+    int Count { get; }
+
+    int Pages { get; }
+
+    IList<T> Items { get; }
+
+    bool HasPrevious { get; }
+
+    bool HasNext { get; }
+}
