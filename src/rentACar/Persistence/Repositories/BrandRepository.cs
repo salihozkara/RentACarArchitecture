f8255942e87159a0a1087a0978bf5bf395a4cde4
Salih <salihozkara00@gmail.com>
2022-03-01 20:57:11 +0300

    create BrandRepository.cs

---
+++ src/rentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+
+namespace Persistence.Repositories;
+
+public class BrandRepository:EfRepositoryBase<Brand,BaseDbContext>,IBrandRepository
+{
+    public BrandRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
