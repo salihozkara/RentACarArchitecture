28173ee9e62f9b6e406b3de4ad50d7fb6874928d
Salih <salihozkara00@gmail.com>
2022-03-01 19:57:19 +0300

    Create BusinessException.cs and BrandBusinessRules.cs

---
+++ src/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+
+namespace Application.Features.Brands.Rules;
+
+public class BrandBusinessRules
+{
+    IBrandRepository _brandRepository;
+    public BrandBusinessRules(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+
+    public async Task BrandNameCanNotBeDuplicatedWhenInserting(string name)
+    {
+        var result = await _brandRepository.GetListAsync(b=>b.Name == name);
+        if(result.Items.Any())
+        {
+            throw new BusinessException("Brand name exists");
+        }
+    }
+}
