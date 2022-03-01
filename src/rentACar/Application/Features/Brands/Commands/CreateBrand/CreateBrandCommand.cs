ebf29a8f9d9b407da5354fb8c1d83c6acd0ca4a9
Salih <salihozkara00@gmail.com>
2022-03-01 18:58:44 +0300

    create CreateBrandCommand.cs

---
+++ src/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CreateBrand;
+
+public class CreateBrandCommand:IRequest<Brand>
+{
+    public string Name { get; set; }
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, Brand>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+        
+
+        public async Task<Brand> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            var mappedBrand = _mapper.Map<Brand>(request);
+            var createdBrand= await _brandRepository.AddAsync(mappedBrand);
+            return createdBrand;
+        }
+    }
+   
+}
