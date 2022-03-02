909921fb12d795bf1e4f312786870c56e0a506e6
Salih <salihozkara00@gmail.com>
2022-03-02 14:24:55 +0300

    create ModelRepository.cs

---
+++ src/rentACar/Persistence/Repositories/ModelRepository.cs
@@ -0,0 +1,13 @@
+using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Context;
+
+namespace Persistence.Repositories;
+
+public class ModelRepository:EfRepositoryBase<Model,BaseDbContext>,IModelRepository
+{
+    public ModelRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
