06b0abe103aeb29880ed05e69b25c08cf83d22b7
Salih <salihozkara00@gmail.com>
2022-03-02 13:39:55 +0300

    add Property car state in Car.cs

--- src/rentACar/Domain/Entities/Car.cs
+++ src/rentACar/Domain/Entities/Car.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Persistence.Repositories;
+using Domain.Enums;
 
 namespace Domain.Entities;
 
@@ -8,19 +9,21 @@ public class Car:Entity
     {
     }
     
-    public Car(int id, int modelId, string plate, short modelYear, int colorId) : this()
+    public Car(int id, int modelId, string plate, short modelYear, int colorId,CarState carState) : this()
     {
         Id = id;
         ModelId = modelId;
         Plate = plate;
         ModelYear = modelYear;
         ColorId = colorId;
+        CarState = carState;
     }
 
     public int ModelId { get; set; }
     public string Plate { get; set; }
     public short ModelYear { get; set; }
     public int ColorId { get; set; }
+    public CarState CarState { get; set; }
 
     public virtual Color Color { get; set; }
     public virtual Model Model { get; set; }
