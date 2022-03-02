977f489c3499996f3c1f8973ec68e7c7721dcf42
Salih <salihozkara00@gmail.com>
2022-03-02 14:32:51 +0300

    create Module MappingProfiles.cs

---
+++ src/rentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,13 @@
+using Application.Features.Models.Commands.CreateModel;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Models.Profiles;
+
+public class MappingProfiles:Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Model, CreateModelCommand>().ReverseMap();
+    }
+}
