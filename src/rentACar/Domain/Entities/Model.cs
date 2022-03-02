e5a3f28b5dd9e2606f08e31eeda31cb674d7528b
Salih <salihozkara00@gmail.com>
2022-03-02 12:25:23 +0300

    In constructor, cars are marked as new hashset

--- src/rentACar/Domain/Entities/Model.cs
+++ src/rentACar/Domain/Entities/Model.cs
@@ -2,11 +2,11 @@
 
 namespace Domain.Entities;
 
-public class Model:Entity
+public class Model : Entity
 {
     public Model()
     {
-
+        Cars = new HashSet<Car>();
     }
 
     public Model(int id, string name, double dailyPrice, int transmissionId, int fuelId, int brandId, string imageUrl)
@@ -26,7 +26,7 @@ public class Model:Entity
     public int FuelId { get; set; }
     public int BrandId { get; set; }
     public string ImageUrl { get; set; }
-    
+
     public virtual Brand Brand { get; set; }
     public virtual Fuel Fuel { get; set; }
     public virtual Transmission Transmission { get; set; }
