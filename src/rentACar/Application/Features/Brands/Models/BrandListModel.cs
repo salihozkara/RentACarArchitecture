0c653b8b92a8b69e93d4e867a5a70959f849a138
Salih <salihozkara00@gmail.com>
2022-03-03 12:04:08 +0300

    BrandListModel.cs created

---
+++ src/rentACar/Application/Features/Brands/Models/BrandListModel.cs
@@ -0,0 +1,9 @@
+using Application.Features.Brands.Dtos;
+using Core.Persistence.Paging;
+
+namespace Application.Features.Brands.Models
+{
+    public class BrandListModel:BasePageableModel<BrandListDto>
+    {
+    }
+}
