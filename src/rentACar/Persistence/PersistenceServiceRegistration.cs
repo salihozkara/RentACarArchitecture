ec3bd931118afaee369d2f3fa09afbc08cda12f3
Salih <salihozkara00@gmail.com>
2022-03-04 11:37:14 +0300

    added scopes of other repositories

--- src/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -12,10 +12,23 @@ public static class PersistenceServiceRegistration
     public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
         IConfiguration configuration)
     {
+        // Add the database context
         services.AddDbContext<BaseDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("rentACarConnectionString")!));
+        
+        // Repositories
+        // Brand
         services.AddScoped<IBrandRepository, BrandRepository>();
+        // Car
+        services.AddScoped<ICarRepository, CarRepository>();
+        // Color
+        services.AddScoped<IColorRepository, ColorRepository>();
+        // Fuel
+        services.AddScoped<IFuelRepository, FuelRepository>();
+        // Model
         services.AddScoped<IModelRepository, ModelRepository>();
+        // Transmission
+        services.AddScoped<ITransmissionRepository, TransmissionRepository>();
 
         return services;
     }
