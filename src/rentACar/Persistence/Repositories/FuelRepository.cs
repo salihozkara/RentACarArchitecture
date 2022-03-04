cf3903798b958dd33c42cbfecfc5d98b387c3c6c
Salih <salihozkara00@gmail.com>
2022-03-04 11:32:40 +0300

    FuelRepository.cs created

---
+++ src/rentACar/Persistence/Repositories/FuelRepository.cs
@@ -0,0 +1,13 @@
+using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Context;
+
+namespace Persistence.Repositories;
+
+public class FuelRepository:EfRepositoryBase<Fuel,BaseDbContext>,IFuelRepository
+{
+    public FuelRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
