18a7f6dded93d7280fdceb530b38c2ab78172df1
Salih <salihozkara00@gmail.com>
2022-03-04 11:56:08 +0300

    fix CreateCarCommand.cs

--- src/rentACar/Application/Features/Cars/Commands/CreateCar/CreateCarCommand.cs
+++ src/rentACar/Application/Features/Cars/Commands/CreateCar/CreateCarCommand.cs
@@ -8,16 +8,17 @@ namespace Application.Features.Cars.Commands.CreateCar;
 
 public class CreateCarCommand:IRequest<Car>
 {
+    // Car Properties
+    public int ColorId { get; set; }
+    public int ModelId { get; set; }
+    public int RentalBranchId { get; set; }
+    public CarState CarState { get; set; }
+    public int Kilometer { get; set; }
+    public short ModelYear { get; set; }
+    public string Plate { get; set; }
     public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, Car>
     {
-        // Car Properties
-        public int ColorId { get; set; }
-        public int ModelId { get; set; }
-        public int RentalBranchId { get; set; }
-        public CarState CarState { get; set; }
-        public int Kilometer { get; set; }
-        public short ModelYear { get; set; }
-        public string Plate { get; set; }
+        
         
         // Car Repository
         private readonly ICarRepository _carRepository;
