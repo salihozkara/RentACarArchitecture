14aba758c20a280e820960b3a71e16b2dc462529
Salih <salihozkara00@gmail.com>
2022-03-04 11:32:07 +0300

    CarRepository.cs created

---
+++ src/rentACar/Persistence/Repositories/CarRepository.cs
@@ -0,0 +1,13 @@
+using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Context;
+
+namespace Persistence.Repositories;
+
+public class CarRepository : EfRepositoryBase<Car, BaseDbContext>, ICarRepository
+{
+    public CarRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
