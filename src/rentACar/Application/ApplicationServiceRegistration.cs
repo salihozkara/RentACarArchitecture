76136d54eef4aaa930e0eed1b9c364314e355f95
Salih <salihozkara00@gmail.com>
2022-03-04 11:36:35 +0300

    comment line added

--- src/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/rentACar/Application/ApplicationServiceRegistration.cs
@@ -10,12 +10,14 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddMediatR(Assembly.GetExecutingAssembly());
+        // Business Rules
         services.AddScoped<BrandBusinessRules>();
         services.AddScoped<ModelBusinessRules>();
-        
-        
+
+
         return services;
     }
 }
