32dea25ce87c654b622a0943b393a0f5fd7af0cb
Salih <salihozkara00@gmail.com>
2022-03-04 15:04:13 +0300

    update UpdateCarCommand.cs

--- src/rentACar/Application/Features/Cars/Commands/UpdateCar/UpdateCarCommand.cs
+++ src/rentACar/Application/Features/Cars/Commands/UpdateCar/UpdateCarCommand.cs
@@ -12,9 +12,7 @@ public class UpdateCarCommand:IRequest<Car>
     public int Id { get; set; }
     public int ColorId { get; set; }
     public int ModelId { get; set; }
-    public int RentalBranchId { get; set; }
     public CarState CarState { get; set; }
-    public int Kilometer { get; set; }
     public short ModelYear { get; set; }
     public string Plate { get; set; }
     
